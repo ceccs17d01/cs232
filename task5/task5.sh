3c2a6926e4f890cf548a57d5f476d3d5c4ea934e
ceccs17d01 <43025232+ceccs17d01@users.noreply.github.com>
2019-05-09 11:11:19 +0530

    Add files via upload

---
+++ task5/task5.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+sftp cs17d01@192.168.0.30
+ls
+put ceccs17d4.2.19.txt
+
+put ceccs17d1.3.19.txt
+put ceccs17d25.2.19.txt
+get ceccs17d25.2.19.txt
+
+size
+status
+system
+
+pwd
+
+exit
